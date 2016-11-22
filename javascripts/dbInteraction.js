993fd8a05ef13332481c5a67434508ade89ee8c6
Rico <richardthomaswhitfield@gmail.com>
2016-11-22 09:19:08 -0600

    db interaction file

---
+++ javascripts/dbInteraction.js
@@ -0,0 +1,21 @@
+"use strict";
+
+let firebase = require("./fb-config");
+
+function searchOMDB(title) {
+	return new Promise(function(resolve,reject) {
+		$.ajax({
+			url: `http://www.omdbapi.com/?t="${title}"&y=&plot=short&r=json`
+		}).done(function(movieData) {
+			console.log("movieData", movieData);
+			console.log("movieData.Poster", movieData.Poster);
+			console.log("movieData.Title", movieData.Title);
+			console.log("movieData.Year", movieData.Year);
+			console.log("movieData.Actors", movieData.Actors);
+			console.log("movieData.Plot", movieData.Plot);
+			resolve(movieData);
+		});
+	});
+}
+
+module.exports = {searchOMDB};
