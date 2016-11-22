382b12f7840698c481e54a65811705a436cbb4c6
Samuel Phillips <samphillips1879@gmail.com>
2016-11-22 14:39:46 -0600

    showing ALL results

--- dist/app.js
+++ dist/app.js
@@ -6,20 +6,52 @@ let firebase = require("./fb-config"),
 	user = require("./user.js");
 
 function searchOMDB(title) {
+	let sumArray = [];
 	return new Promise(function(resolve,reject) {
 		$.ajax({
 			url: `http://www.omdbapi.com/?s="${title}"&y=&plot=short&r=json`
 		}).done(function(movieData) {
-			console.log("movieData from searchOMDB", movieData);
+			// console.log("movieData from searchOMDB", movieData);
 			// console.log("movieData.Poster", movieData.Poster);
 			// console.log("movieData.Title", movieData.Title);
 			// console.log("movieData.Year", movieData.Year);
 			// console.log("movieData.Actors", movieData.Actors);
 			// console.log("movieData.Plot", movieData.Plot);
 			resolve(movieData);
-			cards.cardBuilder(movieData.Search);
+			let currentUser = user.getUser();
+			// console.log("currentUser during search", currentUser);
+			let OMDBArray = movieData.Search;
+			// sumArray = sumArray.concat(OMDBArray);
+			// console.log("sumArray after omdb", sumArray);
+			let firebaseResults = new Promise((resolve, reject)=>{
+				$.ajax({
+					url: `https://moviehistory-f323f.firebaseio.com/movies.json?orderBy="uid"&equalTo="${currentUser}"`
+				}).done((firebaseMovies)=>{
+					console.log("firebaseMovies", firebaseMovies);
+					resolve(firebaseMovies);
+
+					let fbArray = $.map(firebaseMovies, function(value, index) {
+					    return [value];
+					});
+
+					// sumArray = sumArray.concat(firebaseMovies);
+					sumArray = sumArray.concat(fbArray);
+					console.log("sumArray after fb", sumArray);
+
+					sumArray = sumArray.concat(OMDBArray);
+					console.log("sumArray after omdb", sumArray);
+					
+					cards.cardBuilder(sumArray);
+
+					// sumArray = sumArray.concat(firebaseMovies);
+				});
+			});
+    		});
+
+
+
 		});
-	});
+	
 }
 
 function searchID(ID) {
@@ -91,7 +123,6 @@ function getKey() {
 }
 
 module.exports = getKey;
-
 },{}],4:[function(require,module,exports){
 "use strict";
 
@@ -205,6 +236,7 @@ const OUTPUT = $("#movieOutput");
 
 let cards = {};
 cards.cardBuilder = (movieData) => {
+	console.log("movieData", movieData);
 	OUTPUT.html('');
 	let cardsString = '',
 		outputString = '';
--- javascripts/dbInteraction.js
+++ javascripts/dbInteraction.js
@@ -5,20 +5,52 @@ let firebase = require("./fb-config"),
 	user = require("./user.js");
 
 function searchOMDB(title) {
+	let sumArray = [];
 	return new Promise(function(resolve,reject) {
 		$.ajax({
 			url: `http://www.omdbapi.com/?s="${title}"&y=&plot=short&r=json`
 		}).done(function(movieData) {
-			console.log("movieData from searchOMDB", movieData);
+			// console.log("movieData from searchOMDB", movieData);
 			// console.log("movieData.Poster", movieData.Poster);
 			// console.log("movieData.Title", movieData.Title);
 			// console.log("movieData.Year", movieData.Year);
 			// console.log("movieData.Actors", movieData.Actors);
 			// console.log("movieData.Plot", movieData.Plot);
 			resolve(movieData);
-			cards.cardBuilder(movieData.Search);
+			let currentUser = user.getUser();
+			// console.log("currentUser during search", currentUser);
+			let OMDBArray = movieData.Search;
+			// sumArray = sumArray.concat(OMDBArray);
+			// console.log("sumArray after omdb", sumArray);
+			let firebaseResults = new Promise((resolve, reject)=>{
+				$.ajax({
+					url: `https://moviehistory-f323f.firebaseio.com/movies.json?orderBy="uid"&equalTo="${currentUser}"`
+				}).done((firebaseMovies)=>{
+					console.log("firebaseMovies", firebaseMovies);
+					resolve(firebaseMovies);
+
+					let fbArray = $.map(firebaseMovies, function(value, index) {
+					    return [value];
+					});
+
+					// sumArray = sumArray.concat(firebaseMovies);
+					sumArray = sumArray.concat(fbArray);
+					console.log("sumArray after fb", sumArray);
+
+					sumArray = sumArray.concat(OMDBArray);
+					console.log("sumArray after omdb", sumArray);
+					
+					cards.cardBuilder(sumArray);
+
+					// sumArray = sumArray.concat(firebaseMovies);
+				});
+			});
+    		});
+
+
+
 		});
-	});
+	
 }
 
 function searchID(ID) {
