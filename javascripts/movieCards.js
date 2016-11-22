d00c5e957d35b1721f2c62120431f4c68f2dfab6
Samuel Phillips <samphillips1879@gmail.com>
2016-11-22 11:08:52 -0600

    added data--imdb-id and have seen and want to see buttons

--- javascripts/movieCards.js
+++ javascripts/movieCards.js
@@ -17,13 +17,16 @@ cards.cardBuilder = (movieData) => {
 	}
 	// console.log("movieArray", movieArray);
 	movieData.forEach((value, index) => {
-		// console.log("value", value);
+		console.log("value", value);
 		console.log("index", index);
 		if(index % 3 === 0){ 
 			cardsString = `<div class="row">`;
 		}
 
-		cardsString += `<div id="movieCard--${index}" class="col-md-4 movieCard"><h2>${value.Title}</h2><img class="moviePoster" src="${value.Poster}"></div>`;
+		cardsString += `<div id="movieCard--${index}" data--imdb-id="${value.imdbID}" class="col-md-4 movieCard"><h2>${value.Title}</h2><img class="moviePoster" src="${value.Poster}"><div class="btn-group btn-group-justified">
+	      <a id="haveSeen--${index}" href="#" class="btn btn-primary">Have Seen</a>
+	      <a id="wantToSee--${index}" href="#" class="btn btn-primary">Want To See</a>
+	    </div></div>`;
 		
 		if ((index + 1) % 3 === 0) {
 			cardsString += `</div>`;
